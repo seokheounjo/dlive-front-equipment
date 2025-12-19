b2ffe29fd716aeee7b9a6a97e0b51f8d794fe2b2
seokheounjo <untjrgus@naver.com>
2025-12-19 16:54:58 +0900

    Reapply "fix: Vite 프록시 설정 추가 및 API_BASE 수정"
    This reverts commit e04589aee4517e46b7ddd78f89ae4a8b86d0d835.

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,13 @@ export default defineConfig(({ mode }) => {
       server: {
         port: 3000,
         host: '0.0.0.0',
+        proxy: {
+          '/api': {
+            target: 'http://52.63.232.141',
+            changeOrigin: true,
+            secure: false,
+          }
+        }
       },
       plugins: [react()],
       define: {
