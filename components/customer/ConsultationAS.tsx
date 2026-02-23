71471462639ac385b323bb65f8e345fed2e186ea
seokheounjo <untjrgus@naver.com>
2026-02-23 17:35:29 +0900

    fix: non-subscriber AS PG_GUBUN 0->1 for correct status

--- components/customer/ConsultationAS.tsx
+++ components/customer/ConsultationAS.tsx
@@ -596,7 +596,7 @@ const ConsultationAS: React.FC<ConsultationASProps> = ({
         SCHD_DT: asForm.schdDt.replace(/-/g, ''),
         SCHD_TM: asForm.schdHour + asForm.schdMin,
         WRKR_ID: userInfo.userId || '',
-        PG_GUBUN: '0',
+        PG_GUBUN: isSubscriber ? '0' : '1',
         SO_ID: isSubscriber
           ? (selectedContract?.soId || '')
           : (fallbackContract?.SO_ID || userInfo.authSoList?.[0]?.SO_ID || userInfo.authSoList?.[0]?.soId || userInfo.soId || userInfo.SO_ID || ''),
